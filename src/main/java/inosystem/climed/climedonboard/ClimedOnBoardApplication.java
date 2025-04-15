6c55f6129cc96cbeac94550e4efa1d7ae5793643
jrpbnt <jrpbnt@gmail.com>
2025-04-15 15:30:47 -0300

    teste

---
+++ src/main/java/inosystem/climed/climedonboard/ClimedOnBoardApplication.java
@@ -0,0 +1,13 @@
+package inosystem.climed.climedonboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimedOnBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClimedOnBoardApplication.class, args);
+    }
+
+}
